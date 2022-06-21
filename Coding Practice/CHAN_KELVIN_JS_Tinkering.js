2d0eac73bdc58be160c144d3b8f162b392713cf1
Kelvin Chan <kelvin.chan131@gmail.com>
2022-06-21 11:11:27 -0700

    Delete Coding Practice directory

--- Coding Practice/CHAN_KELVIN_JS_Tinkering.js
+++
@@ -1,9 +0,0 @@
-var childHeight = 55
-function displayIfChildIsAbleToRideTheRollerCoaster () {
-    if (childHeight > 52) {
-        console.log("Get on that ride, kiddo!")
-    }
-    else {
-        console.log("sorry kiddo. Maybe next year.")
-    }
-}
